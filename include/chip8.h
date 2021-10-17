e9731c01abcf2ff6a32edffdb63a63b921fd6a6a
Mukesh Kumar Chaurasiya <mukeshmike9@gmail.com>
2021-10-18 05:26:08 +0530

    Registers implemented

--- include/chip8.h
+++ include/chip8.h
@@ -2,10 +2,12 @@
 #define CHIP8_H
 #include "config.h"
 #include "chip8mem.h"
+#include "chip8registers.h"
 
 struct chip8
 {
     struct chip8memory memory;
+    struct chip8registers registers;
 };
 
 #endif
